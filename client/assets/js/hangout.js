1fee52f6327372e7703d8b7fda7ac579a796143e
Niels van Velzen <git@ndat.nl>
2016-10-04 13:49:37 +0200

    Make it harder to abuse the script

--- client/assets/js/hangout.js
+++ client/assets/js/hangout.js
@@ -1,110 +1,112 @@
-var server = 'wss://hangout.ndat.nl/ws/';
-var socket;
-var metadata = {
-	index: null,
-	indexes: [],
-	properties: {}
-};
-var properties = {};
-
-function connect() {
-	socket = new WebSocket(server);
-
-	socket.addEventListener('open', function () {
-		console.log('Connection opened');
-	});
-
-	socket.addEventListener('message', function (event) {
-		try {
-			var json = JSON.parse(event.data);
-
-			json.from = json.from || 'undefined';
-			json.type = json.type || 'ping';
-			json[json.type] = json[json.type] || {};
-
-			handlePacket(json.from, json.type, json[json.type]);
-
-			console.log('IN', json.type);
-		} catch (e) {
-			console.error(e);
-		}
-	});
-
-	socket.addEventListener('close', function (event) {
-		console.log('Connection closed');
-
-		setTimeout(function () {
-			connect();
-		}, 100);
-	});
-}
-
-connect();
-
-function setProperty(property, value) {
-	let data = {};
-	data[property] = value;
-
-	send('server', 'properties', data);
-}
-
-function getProperty(from, property, fallback) {
-	return from in metadata.properties && property in metadata.properties[from] ? metadata.properties[from][property] : fallback;
-}
-
-function send(to, type, data) {
-	var message = {
-		to: to,
-		packet: {
-			type: type
-		}
+var hangout = function(){
+	var server = 'wss://hangout.ndat.nl/ws/';
+	var socket;
+	var metadata = {
+		index: null,
+		indexes: [],
+		properties: {}
 	};
+	var properties = {};
 
-	message['packet'][type] = data;
-	socket.send(JSON.stringify(message));
-}
+	function connect() {
+		socket = new WebSocket(server);
 
-function handlePacket(from, type, data) {
-	switch (type) {
-		case 'metadata':
-			if (metadata.version !== undefined && data.version !== metadata.version)
-				window.location.reload(true);
+		socket.addEventListener('open', function () {
+			console.log('Connection opened');
+		});
 
-			metadata = data;
-			document.getElementById('debug').textContent = JSON.stringify(data, undefined, '    ');
-			break;
+		socket.addEventListener('message', function (event) {
+			try {
+				var json = JSON.parse(event.data);
 
-		case 'draw':
-			context.beginPath();
-			context.fillStyle = getProperty(from, 'style', 'black');
-			var shape = data.shape || 'circle';
+				json.from = json.from || 'undefined';
+				json.type = json.type || 'ping';
+				json[json.type] = json[json.type] || {};
 
-			switch (shape) {
-				case 'circle':
-					context.arc(data.x - 5 || 0, data.y - 5 || 0, 10, 0, 2 * Math.PI);
-					break;
+				handlePacket(json.from, json.type, json[json.type]);
 
-				case 'rectangle':
-					context.rect(data.x - 5 || 0, data.y - 5 || 0, 10, 10);
+				console.log('IN', json.type);
+			} catch (e) {
+				console.error(e);
 			}
-			context.fill();
-			break;
+		});
+
+		socket.addEventListener('close', function (event) {
+			console.log('Connection closed');
+
+			setTimeout(function () {
+				connect();
+			}, 100);
+		});
 	}
-}
 
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
+	connect();
 
-window.addEventListener('load', function () {
-	canvas.width = canvas.offsetWidth;
-	canvas.height = canvas.offsetHeight;
+	function setProperty(property, value) {
+		let data = {};
+		data[property] = value;
 
-	canvas.addEventListener('click', function (event) {
-		console.log(event);
-		send('*', 'draw', {
-			shape: 'circle',
-			x: event.pageX,
-			y: event.pageY
-		})
+		send('server', 'properties', data);
+	}
+
+	function getProperty(from, property, fallback) {
+		return from in metadata.properties && property in metadata.properties[from] ? metadata.properties[from][property] : fallback;
+	}
+
+	function send(to, type, data) {
+		var message = {
+			to: to,
+			packet: {
+				type: type
+			}
+		};
+
+		message['packet'][type] = data;
+		socket.send(JSON.stringify(message));
+	}
+
+	function handlePacket(from, type, data) {
+		switch (type) {
+			case 'metadata':
+				if (metadata.version !== undefined && data.version !== metadata.version)
+					window.location.reload(true);
+
+				metadata = data;
+				document.getElementById('debug').textContent = JSON.stringify(data, undefined, '    ');
+				break;
+
+			case 'draw':
+				context.beginPath();
+				context.fillStyle = getProperty(from, 'style', 'black');
+				var shape = data.shape || 'circle';
+
+				switch (shape) {
+					case 'circle':
+						context.arc(data.x - 5 || 0, data.y - 5 || 0, 10, 0, 2 * Math.PI);
+						break;
+
+					case 'rectangle':
+						context.rect(data.x - 5 || 0, data.y - 5 || 0, 10, 10);
+				}
+				context.fill();
+				break;
+		}
+	}
+
+	var canvas = document.getElementById('canvas');
+	var context = canvas.getContext('2d');
+
+	window.addEventListener('load', function () {
+		canvas.width = canvas.offsetWidth;
+		canvas.height = canvas.offsetHeight;
+
+		canvas.addEventListener('click', function (event) {
+			console.log(event);
+			send('*', 'draw', {
+				shape: 'circle',
+				x: event.pageX,
+				y: event.pageY
+			})
+		});
 	});
-});
+}();
