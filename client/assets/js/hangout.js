9dd0be181a9ffd0ff9e4e89ebc9e122b80071089
Niels van Velzen <git@ndat.nl>
2016-10-07 11:13:42 +0200

    Add /debug command

--- client/assets/js/hangout.js
+++ client/assets/js/hangout.js
@@ -190,6 +190,10 @@ function sendMessage(message) {
 				send('*', 'chat', {type: 'me', text: parts.slice(1).join(' ')});
 				break;
 
+			case 'debug':
+				handlePacket('local', 'chat', {type: 'code', code: JSON.stringify(metadata, undefined, '	')});
+				break;
+
 			default:
 				handlePacket('local', 'chat', {type: 'service', text: 'Unknown command ' + parts[0] + '.'});
 				break;
